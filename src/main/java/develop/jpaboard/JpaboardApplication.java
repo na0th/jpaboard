598730ac13f5547872bee147c85bba28f088d3ae
na0th <105775683+na0th@users.noreply.github.com>
2024-06-13 23:45:59 +0900

    Initial commit

---
+++ src/main/java/develop/jpaboard/JpaboardApplication.java
@@ -0,0 +1,13 @@
+package develop.jpaboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaboardApplication.class, args);
+	}
+
+}
